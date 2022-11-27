824bc9258818fa62f2c038454f36bfa03ae719a2
Dmitrj Zhuravlev <zyzy84@rambler.ru>
2022-11-27 12:46:52 +0300

    finish

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -3,21 +3,35 @@
 
 [3 7 22 2 78] -> 76 */
 
-double [] numbers = {4.5, 1.3, 2.8, 3.5};
+int a = ReadInt ("Введите длину массива: ");
+double [] numbers = new double [a];
+
+Console.Write("[ ");
+for ( int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(0, 99);
+    //numbers[i] = (numbers[i])/10;
+    Console.Write(numbers[i] + " ");
+}
+
 double max = numbers[0];
 double min = numbers[0];
-int i = 0;
-while (i < numbers.Length)
+for (int i = 0; i < numbers.Length; i++)
 {
-    if (numbers[i] > max)
+    if (max < numbers[i])
     {
-    max = numbers[i];
+        max = numbers[i];
     }
-    else if (numbers[i]< min)
+    if (min > numbers[i])
     {
         min = numbers[i];
-    }   
-    i++;
+    }
 }
 double b = max - min;
+Console.Write("] -> ");
 Console.Write($"max = {max}; min = {min}; разница = {b} ");
+int ReadInt (string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
