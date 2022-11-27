387db62d70bb4b7e5d5fe3b28c15bda4bd076d27
Dmitrj Zhuravlev <zyzy84@rambler.ru>
2022-11-27 12:52:26 +0300

    finish3

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -10,7 +10,7 @@ Console.Write("[ ");
 for ( int i = 0; i < numbers.Length; i++)
 {
     numbers[i] = new Random().Next(0, 99);
-    //numbers[i] = (numbers[i])/10;
+    numbers[i] = (numbers[i])/10;
     Console.Write(numbers[i] + " ");
 }
 
